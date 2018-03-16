eb7ae842efe13aaf87f5060dc37b1fd9f2b55339
Anand Jayabalan <ajayabalan@sapient.com>
2018-03-16 13:30:47 -0400

    added initial schema

---
+++ src/main/java/com/graphql/hackernews/GraphQLEndpoint.java
@@ -0,0 +1,14 @@
+package com.graphql.hackernews;
+
+import com.coxautodev.graphql.tools.SchemaParser;
+import graphql.servlet.SimpleGraphQLServlet;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet(urlPatterns = "/graphql")
+public class GraphQLEndpoint extends SimpleGraphQLServlet {
+
+    public GraphQLEndpoint() {
+        super(SchemaParser.newParser().file("schema.graphqls").build().makeExecutableSchema());
+    }
+}
